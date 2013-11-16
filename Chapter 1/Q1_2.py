0d99f100371763ae44093477779c07327dd15f66
Aimee <aimee.hlj@gmail.com>
2013-11-16 15:32:22 -0800

    add recursive implementation

--- Chapter 1/Q1_2.py
+++ Chapter 1/Q1_2.py
@@ -1,10 +1,22 @@
+# using python slice
 def reverse_str(string):
-	return string[::-1]
+    return string[::-1]
+
+# recursive implementation
+def reverse_str_rec(s):
+    if len(s) == 0:
+        return ""
+    if len(s) == 1:
+        return s
+    elif len(s) == 2:
+        return s[-1] + s[0]
+    else:
+        return s[-1] + reverse_str_rec(s[1:-1]) + s[0]
 
 
 #--------------------test-------------------
 inputstr = "abcdefghijk"
-if reverse_str(inputstr)=="kjihgfedcba":
-	print "test passed"
+if reverse_str(inputstr)==reverse_str_rec(inputstr)=="kjihgfedcba":
+    print "test passed"
 else:
-	print "failed"
+    print "failed"
